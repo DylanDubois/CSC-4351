232aa31d74c59debf42d4eebd4cf433c971feb65
Christopher Ross <cross22@lsu.edu>
2018-03-12 20:03:32 -0500

    Update FunEntry.java

--- prog3/Semant/FunEntry.java
+++ prog3/Semant/FunEntry.java
@@ -3,7 +3,7 @@ package Semant;
 public class FunEntry extends Entry {
   public Types.RECORD formals;
   public Types.Type result;
-  CoolEntry(Types.RECORD f, Types.Type r) {
+  FunEntry(Types.RECORD f, Types.Type r) {
     formals = f;
     result = r;
   }
