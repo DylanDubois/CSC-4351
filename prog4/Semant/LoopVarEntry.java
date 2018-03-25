51f1ed5d19042d95b11d8ec65e39a7418213b292
Christopher Ross <cross22@lsu.edu>
2018-03-24 20:13:30 -0500

    Update LoopVarEntry.java

--- prog4/Semant/LoopVarEntry.java
+++ prog4/Semant/LoopVarEntry.java
@@ -1,10 +1,7 @@
 package Semant;
 
 class LoopVarEntry extends VarEntry {
-  LoopVarEntry(Types.Type t) {
-    super(t);
-  }
-  LoopVarEntry(Translate.Access a , Types.Type t){
-	  super(a,t);
+  LoopVarEntry(Types.Type t, Translate.Access a) {
+    super(t,a);
   }
 }
